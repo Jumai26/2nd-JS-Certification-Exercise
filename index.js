825a341af1e06eb69f65e264db3aeed31f745697
Gloria Jumai Williams <willyabiodun@gmail.com>
2024-04-01 14:44:57 +0100

    javascript code updated

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const inputDisplay = document.getElementById('input-display');
 const btn = document.getElementById('btn');
 const result = document.getElementById('result');
+const errorMessage = document.getElementById('error-message')
 
-function converstion() {
+result.style.display = 'none';
+
+function conversion(number) {
+
+    if (inputDisplay.value < 1) {
+        errorMessage.innerText = "Input a valid number"
+        result.innerHTML = "-";
+    } 
+    
     romanNumerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
-    arabicNumerals = [ "M", "CM", "D", "CD", "C", "XC", "L" , "XL", "X", "IX" , "V", "IV", "I"]
+    arabicNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    let roman = "";
+
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (number >= romanNumerals[i]) {
+            roman += arabicNumerals[i];
+            number -= romanNumerals[i];
+        }
+    }
+    return roman;
 };
 
-
+btn.addEventListener('click', () => {
+    const input = parseInt(inputDisplay.value);
+    const romanNumeral = conversion(input);
+    result.style.display = 'block';
+    result.textContent = `${inputDisplay.value} is ${romanNumeral}`;
+});
