63a4bdfca6bda1332da9ce051651816037219c18
Gloria Jumai Williams <willyabiodun@gmail.com>
2024-04-01 15:36:20 +0100

    corrected erroer message

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const errorMessage = document.getElementById('error-message')
 result.style.display = 'none';
 
 function conversion(number) {
-
-    if (inputDisplay.value < 1) {
-        errorMessage.innerText = "Input a valid number"
-        result.innerHTML = "-";
-    } 
     
     romanNumerals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
     arabicNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
@@ -29,5 +24,13 @@ btn.addEventListener('click', () => {
     const input = parseInt(inputDisplay.value);
     const romanNumeral = conversion(input);
     result.style.display = 'block';
+
+    if (inputDisplay.value < 1) {
+        errorMessage.innerText = "Invalid number"
+        result.textContent = `Input a number greater than or equal to one`;
+    }  else if (inputDisplay.value >3999) {
+        errorMessage.innerText = "Invalid number"
+        result.textContent = "Input a number less than 4000";
+    } else
     result.textContent = `${inputDisplay.value} is ${romanNumeral}`;
 });
